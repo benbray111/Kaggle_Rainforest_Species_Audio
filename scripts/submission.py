9dd09116889103d671c4152a7bd682625d23831c
Ben Bray <benbray111@gmail.com>
2021-04-13 21:52:31 -0400

    linting, and use of relative paths

--- scripts/submission.py
+++ scripts/submission.py
@@ -1,79 +1,58 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 11 21:47:07 2021
-
-@author: Ben
-"""
-
-
-import logging
-from pathlib import Path
-
-# from cloudpickle import dump
-from pickle import dump
-
+import os
 import click
 import pandas as pd
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
-from tensorflow.python.keras import utils
-from tensorflow import random
 import keras.models
-import numpy as np
-
-
-
-from utility import parse_config, set_logger, load_submission_test_data 
+from utility import parse_config, set_logger, load_submission_test_data
 
 
 @click.command()
 @click.argument("config_file", type=str, default="config.yaml")
 def create_submission(config_file):
-    print('hello world')
+    ##################
+    # configure logger
+    ##################
+    logger = set_logger("./log/submission.log")
 
     ##################
     # Load config from config file
     ##################
-    #logger.info(f"Load config from {config_file}")
+    logger.info(f"Load config from {config_file}")
     config = parse_config(config_file)
+    #get root directory of repository
+    dirname = os.path.dirname(os.path.dirname(__file__))
+    #set local variables from config file
+    model_path = os.path.join(dirname, config["submission"]["model_path"])
+    processed_path = os.path.join(dirname, config["submission"]["processed_path"])
+    submission_file = os.path.join(dirname, config["submission"]["submission_file"])
+    submission_template = os.path.join(dirname, config["submission"]["submission_template"])
 
-    model_path = Path(config["submission"]["model_path"])
-    processed_path = config["submission"]["processed_path"]
-    submission_file = config["submission"]["submission_file"]
-    submission_template = config["submission"]["submission_template"]
-    
     ##################
     # Load trained model
     ##################
-    # Load model
     #logger.info("-------------------Load the trained model-------------------")
-        
     trained_model = keras.models.load_model(model_path)
 
     ##################
     # Load data
     ##################
     #get processed data
+    #this loads both information about each clip, and the spectrograms
     submission_clips_df, X = load_submission_test_data(processed_path)
     #get template for submission
     the_template = pd.read_csv(submission_template)
-    
-    
+
     ##################
     # Make Predictions
-    ##################  
-    
+    ##################
     y_hat = trained_model.predict(X)
-    
-    
     test_set_predictions = pd.DataFrame(y_hat, columns = [x for x in the_template if x != 'recording_id'])
     test_set_predictions = pd.concat([submission_clips_df, test_set_predictions], axis=1)
+    #adjusts for the fact that each clip can test positive for mult species
     test_set_predictions = test_set_predictions.groupby('Row_ID').max()
-    test_set_predictions = test_set_predictions[[x for x in test_set_predictions if x !='Row_ID' and x != 'Clip_Segment']]
+    test_set_predictions = test_set_predictions[[x for x in test_set_predictions if x != 'Row_ID' and x != 'Clip_Segment']]
     test_set_predictions = test_set_predictions.rename(columns={'Clip': 'recording_id'})
+    #export file
     test_set_predictions.to_csv(submission_file, index=False)
-    
-    
 
 if __name__ == "__main__":
     create_submission()
--- scripts/utility.py
+++ scripts/utility.py
@@ -1,19 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Contains utility functions used throughout the other scripts
-"""
-
-import yaml
 import logging
 from pathlib import Path
-import pandas as pd
 import pickle
 import numpy as np
+import pandas as pd
+import yaml
 from keras.utils import to_categorical
-#from sklearn.model_selection import train_test_split
-
-
-
 def parse_config(config_file):
 
     with open(config_file, "rb") as f:
@@ -22,11 +13,7 @@ def parse_config(config_file):
 
 
 def set_logger(log_path):
-    """
-    Read more about logging: https://www.machinelearningplus.com/python/python-logging-guide/
-    Args:
-        log_path [str]: eg: "../log/train.log"
-    """
+
     log_path = Path(log_path)
     log_path.parent.mkdir(parents=True, exist_ok=True)
     logger = logging.getLogger(__name__)
@@ -50,53 +37,43 @@ def load_data(processed_features, processed_labels, train_or_test_subsetter):
     #get the row numbers you want to subset with (either the training or test set)
     with open(train_or_test_subsetter, 'rb') as f:
         subsetter = pickle.load(f)
-    print('printing type')
-    print(type(subsetter))
-    print(type(subsetter[0]))
     with open(processed_features, 'rb') as f:
         features = pickle.load(f)
     #convert features into a numpy array
     features = np.array(features).reshape(len(features), 224, 400, 1)
     #subset the array of features to include just the ones in the subsetter
-    features = features[subsetter,:,:,:]
-
+    features = features[subsetter, :, :, :]
 
     with open(processed_labels, 'rb') as f:
         labels = pickle.load(f)
     labels_df = pd.DataFrame(labels, columns=['Label'])
 
-
     #subset your data
     labels_df = labels_df.iloc[subsetter, :]
     labels_df = to_categorical(labels_df)
-    
     return features, labels_df, ['Spectrogram']
 
 
 def load_submission_test_data(location):
     #create dataframe with submission test data
     #combine references for test into dataframe
-    
+
     with open(str(location)+'//row_ids_test.pickle', 'rb') as f:
         row_ids_test = pickle.load(f)
 
     with open(str(location)+'//clips_test.pickle', 'rb') as f:
         clips_test = pickle.load(f)
-        
+
     with open(str(location)+'//clip_segments_test.pickle', 'rb') as f:
         clip_segments_test = pickle.load(f)
-    
+
     submission_clips_df = pd.DataFrame(columns=['Row_ID', 'Clip', 'Clip_Segment'])
     submission_clips_df['Row_ID'] = row_ids_test
     submission_clips_df['Clip'] = clips_test
     submission_clips_df['Clip_Segment'] = clip_segments_test
-    
+
     with open(str(location)+'//spectrograms_test.pickle', 'rb') as f:
         spectrograms = pickle.load(f)
     spectrograms = np.array(spectrograms).reshape(len(spectrograms), 224, 400, 1)
-    
+
     return submission_clips_df, spectrograms
-    
-    
-    
-    
