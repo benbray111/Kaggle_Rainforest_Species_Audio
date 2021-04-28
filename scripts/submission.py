f49a0a45967fbdd7d22f7d84faded8be454b9a93
Ben Bray <benbray111@gmail.com>
2021-04-28 15:51:50 -0400

    Linting.

--- scripts/submission.py
+++ scripts/submission.py
@@ -1,3 +1,9 @@
+"""Create submission for Kaggle Competition
+
+Based on instructions from Kaggle Rainforest Audio
+https://www.kaggle.com/c/rfcx-species-audio-detection/data
+"""
+
 import os
 import click
 import pandas as pd
@@ -8,6 +14,8 @@ from utility import parse_config, set_logger, load_submission_test_data
 @click.command()
 @click.argument("config_file", type=str, default="config.yaml")
 def create_submission(config_file):
+    """Create and save Kaggle Submission file
+    """
     ##################
     # configure logger
     ##################
@@ -16,7 +24,7 @@ def create_submission(config_file):
     ##################
     # Load config from config file
     ##################
-    logger.info(f"Load config from {config_file}")
+    logger.info("Load config from %s .", config_file)
     config = parse_config(config_file)
     #get root directory of repository
     dirname = os.path.dirname(os.path.dirname(__file__))
@@ -37,22 +45,24 @@ def create_submission(config_file):
     ##################
     #get processed data
     #this loads both information about each clip, and the spectrograms
-    submission_clips_df, X = load_submission_test_data(processed_path)
+    submission_clips_df, x_spectrograms = load_submission_test_data(processed_path)
     #get template for submission
     the_template = pd.read_csv(submission_template)
 
     ##################
     # Make Predictions
     ##################
-    y_hat = trained_model.predict(X)
-    test_set_predictions = pd.DataFrame(y_hat, columns = [x for x in the_template if x != 'recording_id'])
+    y_hat = trained_model.predict(x_spectrograms)
+    test_set_predictions = pd.DataFrame(y_hat, columns = [
+        x for x in the_template if x != 'recording_id'])
     test_set_predictions = pd.concat([submission_clips_df, test_set_predictions], axis=1)
     #adjusts for the fact that each clip can test positive for mult species
     test_set_predictions = test_set_predictions.groupby('Row_ID').max()
-    test_set_predictions = test_set_predictions[[x for x in test_set_predictions if x != 'Row_ID' and x != 'Clip_Segment']]
+    test_set_predictions = test_set_predictions[
+        [x for x in test_set_predictions if x not in ('Row_ID', 'Clip_Segment')]]
     test_set_predictions = test_set_predictions.rename(columns={'Clip': 'recording_id'})
     #export file
     test_set_predictions.to_csv(submission_file, index=False)
 
 if __name__ == "__main__":
-    create_submission()
+    create_submission(None)
